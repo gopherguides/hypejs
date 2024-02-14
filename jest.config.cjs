d2eb997ac69aa4215be26bdb097ccf72e84661f2
Mark Bates <mark@markbates.com>
2024-02-14 14:29:20 -0500

    tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,9 +1,15 @@
 module.exports = {
-    transform: {
-        '^.+\\.ts?$': 'ts-jest',
-    },
-    testEnvironment: 'node',
-    testRegex: './src/.*\\.(test|spec)?\\.(ts|ts)$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    roots: ['<rootDir>/src'],
+  preset: "ts-jest",
+  transform: {
+    "^.+\\.ts?$": "ts-jest",
+  },
+  testEnvironment: "node",
+  testRegex: "./src/.*\\.(test|spec)?\\.(ts|ts)$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  roots: ["<rootDir>/src"],
+  collectCoverage: true,
+  coverageReporters: ["lcov", "text-summary"],
+  collectCoverageFrom: ["src/**/*.ts"],
+  coveragePathIgnorePatterns: ["/node_modules/", "/__tests__/"],
+  testPathIgnorePatterns: ["/node_modules/"],
 };
